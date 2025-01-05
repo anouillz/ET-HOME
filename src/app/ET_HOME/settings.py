4ee5990c642b8187fb37ebe22b012edb5c5e3bf6
Kevin Voisin <kevin.voisin187@gmail.com>
2025-01-05 12:20:48 +0100

    login and register init functionalities

--- src/app/ET_HOME/settings.py
+++ src/app/ET_HOME/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ET_HOME',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -125,6 +126,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATICFILES_DIRS=[
+BASE_DIR / "static"  
+]
 STATIC_URL = 'static/'
 
 # Default primary key field type
